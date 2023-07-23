47a9bcd4acb4cc359934af81ab7c3fece7c3434a
Khang Tran <ktra@email.unc.edu>
2023-07-22 20:47:19 -0400

    add constants

---
+++ constants/canvasConstants.ts
@@ -0,0 +1,15 @@
+const MIN_SCALE: number = 0.2;
+const MAX_SCALE: number = 5;
+const ZOOM_OUT_FACTOR: number = 0.9;
+const ZOOM_IN_FACTOR: number = 1.1;
+const LINE_TOLERANCE: number = 1;
+const CIRCLE_TOLERANCE: number = 0.03;
+
+export {
+  MIN_SCALE,
+  MAX_SCALE,
+  ZOOM_OUT_FACTOR,
+  ZOOM_IN_FACTOR,
+  LINE_TOLERANCE,
+  CIRCLE_TOLERANCE,
+};
